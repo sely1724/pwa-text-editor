3c1e9b02f462c3e154159206566c1b1d02120ad8
Sylvia Ely <sylvianne9417@gmail.com>
2023-02-20 18:28:01 -0800

    Slowly debugging as I watch John's mini project walkthrough

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,49 +1,49 @@
-const { offlineFallback, warmStrategyCache } = require("workbox-recipes"); // HOW DO WE INCORPORATE OFFLINE FALLBACK??
-const { CacheFirst } = require("workbox-strategies");
-const { registerRoute } = require("workbox-routing");
-const { CacheableResponsePlugin } = require("workbox-cacheable-response");
-const { ExpirationPlugin } = require("workbox-expiration");
-const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
+// const { offlineFallback, warmStrategyCache } = require("workbox-recipes"); // HOW DO WE INCORPORATE OFFLINE FALLBACK??
+// const { CacheFirst } = require("workbox-strategies");
+// const { registerRoute } = require("workbox-routing");
+// const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+// const { ExpirationPlugin } = require("workbox-expiration");
+// const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
-precacheAndRoute(self.__WB_MANIFEST);
+// precacheAndRoute(self.__WB_MANIFEST);
 
-const pageCache = new CacheFirst({
-  cacheName: "page-cache",
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
+// const pageCache = new CacheFirst({
+//   cacheName: "page-cache",
+//   plugins: [
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//     new ExpirationPlugin({
+//       maxAgeSeconds: 30 * 24 * 60 * 60,
+//     }),
+//   ],
+// });
 
-warmStrategyCache({
-  urls: ["/index.html", "/"],
-  strategy: pageCache,
-});
+// warmStrategyCache({
+//   urls: ["/index.html", "/"],
+//   strategy: pageCache,
+// });
 
-registerRoute(({ request }) => request.mode === "navigate", pageCache);
+// registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// Set up asset cache
-registerRoute(
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
-  ({ request }) => request.destination === "image",
-  new CacheFirst({
-    // Name of the cache storage.
-    cacheName: "asset-cache",
-    plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
+// // Set up asset cache
+// registerRoute(
+//   // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+//   ({ request }) => request.destination === "image",
+//   new CacheFirst({
+//     // Name of the cache storage.
+//     cacheName: "asset-cache",
+//     plugins: [
+//       // This plugin will cache responses with these headers to a maximum-age of 30 days
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
 
-      new ExpirationPlugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-      }),
-    ],
-  })
-);
-registerRoute();
+//       new ExpirationPlugin({
+//         maxEntries: 60,
+//         maxAgeSeconds: 30 * 24 * 60 * 60,
+//       }),
+//     ],
+//   })
+// );
+// registerRoute();
